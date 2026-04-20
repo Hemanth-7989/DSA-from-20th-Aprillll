6cca0f2032c0c61126c959f99a2d1afd7c2a5ad9
Hemanth <hemanth.mudunuri@gmail.com>
2026-04-20 21:56:07 +0530

    Refactor output formatting and update input/output files; recreate tasks.json for build automation

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        System.out.println("Hey "+ x);
+        System.out.println("Hey " + x);
         scanner.close();
     }
 }
